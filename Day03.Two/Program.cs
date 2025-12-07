ec738f1294bdcdf3b740c93636c14cfae6fe3aff
PJB3005 <pieterjan.briers+git@gmail.com>
2025-12-07 01:22:38 +0100

    Day 3

---
+++ Day03.Two/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using Shared;
+
+var result = Input.Get("day03")
+    .Split("\n")
+    .Select(x => x.Trim())
+    .Where(x => !string.IsNullOrWhiteSpace(x))
+    .AsParallel()
+    .Select(CalculateJoltage)
+    .Sum();
+
+Console.WriteLine(result);
+
+static long CalculateJoltage(string value)
+{
+    const int digits = 12;
+
+    var span = value.AsSpan().Trim();
+
+    var accum = 0L;
+
+    for (var i = 0; i < digits; i++)
+    {
+        var keepChars = digits - i - 1;
+        FindMaxInString(span[..^keepChars], out var digit, out var pos);
+        accum *= 10;
+        accum += digit;
+
+        span = span[(pos+1)..];
+    }
+
+    Console.WriteLine($"Max in {value}: {accum}");
+    return accum;
+}
+
+static void FindMaxInString(ReadOnlySpan<char> span, out int digit, out int pos)
+{
+    Debug.Assert(span.Length > 0);
+
+    var highestPos = -1;
+    var highest = '0';
+
+    for (var i = 0; i < span.Length; i++)
+    {
+        var chr = span[i];
+        Debug.Assert(chr is <= '9' and >= '1');
+        if (chr > highest)
+        {
+            highest = chr;
+            highestPos = i;
+        }
+    }
+
+    Debug.Assert(highestPos >= 0);
+
+    digit = highest - '0';
+    pos = highestPos;
+}
---
+++ Day03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Shared;
+
+var result = Input.Get("day03")
+    .Split("\n")
+    .Select(x => x.Trim())
+    .Where(x => !string.IsNullOrWhiteSpace(x))
+    .AsParallel()
+    .Select(CalculateJoltage)
+    .Sum();
+
+Console.WriteLine(result);
+
+static int CalculateJoltage(string value)
+{
+    value = value.Trim();
+
+    var highestPos = -1;
+    var highest = '0';
+
+    for (var i = 0; i < value.Length - 1; i++)
+    {
+        var chr = value[i];
+        Debug.Assert(chr is <= '9' and >= '1');
+        if (chr > highest)
+        {
+            highest = chr;
+            highestPos = i;
+        }
+    }
+
+    var secondHighest = value[(highestPos + 1)..].Max();
+
+    var max = int.Parse($"{highest}{secondHighest}");
+
+    Console.WriteLine($"Max in {value}: {max}");
+
+    return max;
+}
