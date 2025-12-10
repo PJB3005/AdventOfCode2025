fc1de27e7d71dff0e0d9df9264366d71728e8759
PJB3005 <pieterjan.briers+git@gmail.com>
2025-12-10 02:31:22 +0100

    Day 7

---
+++ Day07.Two/Program.cs
@@ -0,0 +1,72 @@
+﻿// 5137133207829 -> too low
+
+using System.Diagnostics;
+using Shared;
+
+using var input = Input.GetReader("day07");
+
+// Parse file.
+var firstLine = input.ReadLine() ?? throw new Exception();
+
+var startPos = firstLine.IndexOf('S');
+var width = firstLine.Length;
+
+var rows = new List<Tile[]>();
+
+while (input.ReadLine() is { } line)
+{
+    var row = line.Select(chr => new Tile(chr == '^')).ToArray();
+    rows.Add(row);
+}
+
+Debug.Assert(rows.All(row => row.Length == width));
+
+// Propagate bottom to top.
+
+for (var r = rows.Count - 1; r >= 0; r--)
+{
+    var row = rows[r];
+    for (var c = 0; c < row.Length; c++)
+    {
+        var tile = row[c];
+        if (tile.IsSplitter)
+        {
+            PropagateSplitterUp(tile, r, c);
+        }
+    }
+}
+
+var topSplitter = rows[1].Single(x => x.IsSplitter);
+
+var timelineCount = topSplitter.TimelineCount + 1;
+
+Console.WriteLine(timelineCount);
+
+void PropagateSplitterUp(Tile splitter, int startRow, int column)
+{
+    for (var r = startRow - 1; r >= 0; r--)
+    {
+        var tile = rows[r][column];
+        if (tile.IsSplitter)
+            return;
+
+        PropagateIfSplitter(splitter, r, column - 1);
+        PropagateIfSplitter(splitter, r, column + 1);
+    }
+}
+
+void PropagateIfSplitter(Tile splitter, int row, int column)
+{
+    ref var tile = ref rows[row][column];
+    if (!tile.IsSplitter)
+        return;
+
+    tile.TimelineCount += splitter.TimelineCount;
+}
+
+[DebuggerDisplay("Splitter: {IsSplitter} w/ {TimelineCount}")]
+internal struct Tile(bool isSplitter)
+{
+    public readonly bool IsSplitter = isSplitter;
+    public long TimelineCount = isSplitter ? 1 : 0;
+}
---
+++ Day07/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Shared;
+
+using var input = Input.GetReader("day07");
+
+var firstLine = input.ReadLine() ?? throw new Exception();
+
+var startPos = firstLine.IndexOf('S');
+var width = firstLine.Length;
+
+var rows = new List<Tile[]>();
+
+while (input.ReadLine() is { } line)
+{
+    var row = line.Select(chr => chr == '^' ? Tile.Splitter : Tile.Empty).ToArray();
+    rows.Add(row);
+}
+
+Debug.Assert(rows.All(row => row.Length == width));
+
+// Make row below start a beam.
+
+Debug.Assert(rows[0][startPos] == Tile.Empty);
+rows[0][startPos] = Tile.Beam;
+
+var totalSplittersHit = 0;
+
+for (var i = 0; i < rows.Count - 1; i++)
+{
+    var curRow = rows[i];
+    var nextRow = rows[i + 1];
+
+    for (var x = 0; x < curRow.Length; x++)
+    {
+        var tile = curRow[x];
+        if (tile != Tile.Beam)
+            continue;
+
+        var nextTile = nextRow[x];
+        if (nextTile == Tile.Empty)
+        {
+            nextRow[x] = Tile.Beam;
+        }
+        else if (nextTile == Tile.Splitter)
+        {
+            totalSplittersHit += 1;
+            nextRow[x - 1] = Tile.Beam;
+            nextRow[x + 1] = Tile.Beam;
+        }
+    }
+}
+
+Console.WriteLine(totalSplittersHit);
+
+internal enum Tile : byte
+{
+    Empty,
+    Splitter,
+    Beam
+}
